e722b0039f96a9777b394b5a4ecd96ce7ad5127d
AxelTapiaCC <axel.tapia@ciencias.unam.mx>
2026-04-26 18:12:05 -0600

    fix: Arreglo de querys, en nuestra tabla Personal no hay FechaNacimiento, entonces ahi hay un error garrafal, sino se puede pues agregamos ese atributo

--- Práctica07_LosSúperDatos/SQL/Query.sql
+++ Práctica07_LosSúperDatos/SQL/Query.sql
@@ -1,26 +1,33 @@
 -- i. Clientes cuyo nombre empiece con la letra R
 SELECT *
-FROM Cliente
-WHERE Nombre LIKE 'R%';
+FROM CLIENTE
+WHERE Nombre ILIKE 'R%';
 
 
 -- ii. Medicamentos que hayan caducado después del 20 de abril del 2026 pero antes del 07 de mayo del 2026
 SELECT *
-FROM Medicamento
+FROM MEDICAMENTO
 WHERE FechaDeCaducidad > '2026-04-20'
   AND FechaDeCaducidad < '2026-05-07';
 
 
 -- iii. Farmacéuticos que hayan nacido en el mes de noviembre
+SELECT p.*
+FROM FARMACEUTICO AS f
+JOIN PERSONAL AS p
+  ON p.IDPersonal = f.IDPersonal
+WHERE SUBSTRING(p.RFC FROM 7 FOR 2) = '11';
 
 
 -- iv. Medicamentos cuya forma física sea gel y vía de administración sea oral
-SELECT *
-FROM Insumo
-WHERE LOWER(FormaFarmaceutica) = 'gel'
-  AND LOWER(ViaAdministracion) = 'oral';
+SELECT m.*
+FROM MEDICAMENTO AS m
+JOIN INSUMO AS i
+  ON i.NombreCientifico = m.NombreCientifico
+WHERE i.FormaFarmaceutica ILIKE 'gel'
+  AND i.ViaAdministracion ILIKE 'oral';
 
 
 -- v. Todos los proveedores registrados
 SELECT *
-FROM Proveedor;
+FROM PROVEEDOR;
